05510a2e1a2aa19377c6fac31e24355bb9685e86
yasserMokh <yasser.moukhtar@outlook.com>
2022-10-25 10:13:23 +0200

    restrructure & shared module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateEmployeeComponent } from './employee/components/create/create-employee.component';
-import { EditEmployeeComponent } from './employee/components/edit/edit-employee.component';
-import { ListEmployeesComponent } from './employee/components/list/list-employees.component';
+
 
 const routes: Routes = [
-  { path: 'employee.list', component: ListEmployeesComponent },
-  { path: 'employee.create', component: CreateEmployeeComponent },
-  { path: 'employee.edit/:id', component: EditEmployeeComponent },
-  { path: '', redirectTo: '/employee.list', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -16,3 +10,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+ 
